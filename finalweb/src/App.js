7e4defec25d808e6ee099d8120c7a0b085a6b298
juanpernett1299 <juan.pernett@uao.edu.co>
2021-11-16 09:58:04 -0500

    Diagramación y estilos de tablas

--- finalweb/src/App.js
+++ finalweb/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Tabla from './componentes/Tabla/Tabla';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Citas pendientes
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Tabla fecha="18/06/21 08:30" mensaje="Su cita fue pre-aprobada.
+Se ha verificado su formato y cumple con las condiciones"></Tabla>
     </div>
   );
 }
