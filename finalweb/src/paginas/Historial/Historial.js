d208172b625642a1b08f92e96375108a422c7826
Santiago A <santiago.arce@uao.edu.co>
2021-11-18 00:38:47 -0500

    fix: histoirial

--- finalweb/src/paginas/Historial/Historial.js
+++ finalweb/src/paginas/Historial/Historial.js
@@ -5,6 +5,7 @@ import Titulo from "../../componentes/Titulo/Titulo";
 import "./Historial.css"
 import BotonVolver from "../../componentes/CitaHisto/BotonVolver/BotonVolver";
 import CitaHisto from "../../componentes/CitaHisto/CitaHisto";
+import linea from "../../imagenes/linea.jpeg"
 
 const data = {
     titulo: "Cita1",
@@ -24,11 +25,22 @@ class Historial extends Component {
                     <Titulo texto="Historial de Citas"/>
                
                 </div>  
+                <div className="contenedorMes">
+                    <img className="linea" src={linea}></img>
+                    <h1 className="tituloMes">Enero</h1>
+                    <img className="linea" src={linea}></img>
+                </div>
                 <div className="contenedorCitasHisto">
                     <CitaHisto data={data}></CitaHisto>
                     <CitaHisto data={data}></CitaHisto>
                     <CitaHisto data={data}></CitaHisto>
+                    <CitaHisto data={data}></CitaHisto>
                 </div> 
+                <div className="contenedorMes">
+                    <img className="linea" src={linea}></img>
+                    <h1 className="tituloMes">Febrero</h1>
+                    <img className="linea" src={linea}></img>
+                </div>
                 <div className="contenedorCitasHisto">
                     <CitaHisto data={data}></CitaHisto>
                     <CitaHisto data={data}></CitaHisto>
