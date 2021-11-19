a93205a9fa2e93f61a49e73223fb2eb7ab0960c9
Jose Luis <wildchamo@gmail.com>
2021-11-19 16:23:22 -0500

    base html user info

---
+++ finalweb/src/componentes/header/headerInfo/datosUsuario.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./datosUsuario.css";
+import Elipse from "../../../imagenes/ellipse.png";
+import Flecha from "../../../imagenes/flechaheader.png";
+import ModalU from "./modalU.js";
+
+class DatosUsuario extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    return (
+      
+        <div className="datosU" onClick={this.showModal}>
+          <p>Nombre Usuario</p>
+          <img src={Elipse} className="elipse"></img>
+          <img src={Flecha} className="flecha"></img>
+      
+
+        <ModalU
+          data={"restablecer"}
+          show={this.state.show}
+          handleClose={this.hideModal}
+        >
+          <div></div>
+        </ModalU>
+      </div>
+    );
+  }
+}
+
+export default DatosUsuario;
