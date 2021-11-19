e1fd9fcb4e061c78946bb6d03e12cb2b390b5535
Jose Luis <wildchamo@gmail.com>
2021-11-18 20:06:51 -0500

    final modal restablecer

--- finalweb/src/paginas/restablecer/restablecer.js
+++ finalweb/src/paginas/restablecer/restablecer.js
@@ -1,51 +1,63 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Boton from "../../componentes/Boton/Boton";
 import Campo from "../../componentes/Campo/Campo";
-
-import Headerr from "../../componentes/headerr/headerr.js"
+import Modal from "./modal.js";
+import Headerr from "../../componentes/headerr/headerr.js";
 import "./restablecer.css";
 
 class Restablecer extends Component {
-    constructor() {
-        super();
-        this.state = {
-          show: false
-        };
-        this.showModal = this.showModal.bind(this);
-        this.hideModal = this.hideModal.bind(this);
-      }
-    
-      showModal = () => {
-        this.setState({ show: true });
-      };
-    
-      hideModal = () => {
-        this.setState({ show: false });
-      };
-    render(){
-        return(
-            <div className="restablecer">
-                <Headerr/>
-                <div className="restablecerTexto"> 
-                <h2>Restablecer contraseña</h2>
-                <p>Para restablecer su contraseña, envíe su nombre de usuario o su dirección de correo electrónico , le enviaremos un email con instrucciones para poder acceder de nuevo.
-</p>
-                <p>Ingresa tu correo o nombre de usuario</p>
-                <Campo tamaño="campo-s" tipo="texto"/>
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
 
-                {/* <Link to="/">
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+  render() {
+    return (
+      <div className="restablecer">
+        <Headerr />
+        <div className="restablecerTexto">
+          <h2>Restablecer contraseña</h2>
+          <p>
+            Para restablecer su contraseña, envíe su nombre de usuario o su
+            dirección de correo electrónico , le enviaremos un email con
+            instrucciones para poder acceder de nuevo.
+          </p>
+          <p>Ingresa tu correo o nombre de usuario</p>
+          <Campo tamaño="campo-s" tipo="texto" />
+
+          {/* <Link to="/">
                         <Boton tamaño="boton1" titulo="Enviar"></Boton>
                     </Link> */}
-                        <Boton tamaño="boton1" titulo="Enviar"></Boton>
-                </div>
- 
-                
-            </div>
-            
+          {/* <Boton tamaño="boton1" titulo="Enviar" onClick={this.showModal}></Boton> */}
+          <button tamaño="boton1" onClick={this.showModal}>
+            Enviar
+          </button>
+        </div>
 
-            )
-        }
-    }
-    export default Restablecer;
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <h2>Correo enviado con éxito</h2>
+          <p className="parrafoModal">
+            Se ha enviado un email para que puedas restablecer tu contraseña con
+            éxito.
+          </p>
+          <br/>
+          <p>Contiene instrucciones sencillas para confirmar y completar el cambio de contraseña. </p>
+        </Modal>
+      </div>
+    );
+  }
+}
+export default Restablecer;
