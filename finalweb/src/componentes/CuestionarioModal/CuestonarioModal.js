8ddc6de0f37c3773836dae0d9d2167022e7c6865
Santiago A <dev.game7@talentum.edu.co>
2021-11-18 23:18:58 -0500

    cuestionario modal funcionando

--- finalweb/src/componentes/CuestionarioModal/CuestonarioModal.js
+++ finalweb/src/componentes/CuestionarioModal/CuestonarioModal.js
@@ -5,9 +5,9 @@ import TituloModal from "../TituloModal/TituloModal";
 import boton from "../../imagenes/volveratrasrojo.png";
 import Boton from "../Boton/Boton";
 
-const CuestionarioModal = ({isOpenCuest, closeModalCuest}) => {
+const CuestionarioModal = ({isOpen, closeModal}) => {
     return (
-        <ModalGL tipo="cuestionario-modal" isOpen={isOpenCuest} closeModal={closeModalCuest}>
+        <ModalGL tipo="cuestionario-modal" isOpen={isOpen} closeModal={closeModal}>
             <div >
               <TituloModal titulo=" Agenda tu Cita " imagen={boton}></TituloModal>
               <h3>Ayúdanos a conocer más sobre tu problema para conectarte con el psicólogo indicado</h3>
