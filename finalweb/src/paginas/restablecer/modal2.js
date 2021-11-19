d00ebb7388f2492638b2e2d75ee9c21bae945d23
Jose Luis <wildchamo@gmail.com>
2021-11-18 23:58:24 -0500

    fin boton soporte

--- finalweb/src/paginas/restablecer/modal2.js
+++ finalweb/src/paginas/restablecer/modal2.js
@@ -1,7 +1,9 @@
 import './modal2.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import XBlanca from "../../imagenes/Xfondoblanco.png";
-import Campo  from "../../componentes/Campo/Campo.js"
+import Fenviar from "../../imagenes/enviar.png";
+import Campo  from "../../componentes/Campo/Campo.js";
+
 const Modal2 = ({ handleClose, show, children,data }) => {
   const {texto}=data
   const showHideClassName = show ? "modal display-block" : "modal display-none";
@@ -20,8 +22,8 @@ const Modal2 = ({ handleClose, show, children,data }) => {
 
         <div className="Mensajes"> 
         <Campo tamaño="campo-l" tipo="texto" texto="Escribe un mensaje ..."/>
+        <img src={Fenviar}></img>
         </div>
-
       </section>
     </div>
   );
