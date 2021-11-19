8c86f5389524f5e277b6a62a86af31d9545f903c
Jose Luis <wildchamo@gmail.com>
2021-11-18 19:53:45 -0500

    importar las funciones

--- finalweb/src/paginas/restablecer/restablecer.js
+++ finalweb/src/paginas/restablecer/restablecer.js
@@ -8,6 +8,22 @@ import Headerr from "../../componentes/headerr/headerr.js"
 import "./restablecer.css";
 
 class Restablecer extends Component {
+    constructor() {
+        super();
+        this.state = {
+          show: false
+        };
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+      }
+    
+      showModal = () => {
+        this.setState({ show: true });
+      };
+    
+      hideModal = () => {
+        this.setState({ show: false });
+      };
     render(){
         return(
             <div className="restablecer">
